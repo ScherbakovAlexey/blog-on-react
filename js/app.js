7e8d5835c94cb516743fe6516c9fbce5918584f1
ScherbakovAlexey <scherbakovalexey@mail.ru>
2019-01-20 11:57:08 +0300

    fetch from jsonplaceholder, more button

--- js/app.js
+++ js/app.js
@@ -1,71 +1,69 @@
-var my_news = [
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+			num: 3
+    };
+			this.handleClick = this.handleClick.bind(this);
   }
-];
+	
 
-var Article = React.createClass({
-  render: function() {
-    var title = this.props.data.title,
-        body = this.props.data.body;
 
-    return (
-      <div className='article'>
-        <p className='news__title'>{title}:</p>
-				<p className='news__date'> MARCH 2, 2016 | TRAVEL</p>
-        <p className='news__body'>{body}</p>
-			  <div className='news__btn'>Continue reading</div>
-      </div>
-    )
+  componentDidMount() {
+		//alert('n');
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+		.then(results => {
+			return results.json();
+		}).then(data => {
+			let posts = data.map((pst) => {
+				console.log(pst.title);
+				console.log(pst.userId+pst.id);
+				return(
+					<div key={pst.userId+pst.id} className='article'>
+						<p className='news__title'>{pst.title}</p>
+						<p className='news__date'> MARCH 2, 2016 | TRAVEL</p>
+						<p className='news__body'>{pst.body}</p>
+						<div className='news__btn'>Continue reading</div>
+					</div>
+				) 
+			})
+			this.setState({posts: posts});
+			console.log("state", this.state.posts);
+		})
   }
-});
 
-var News = React.createClass({
-  render: function() {
-    var data = this.props.data;
-    var newsTemplate;
-
-    if (data.length > 0) {
-      newsTemplate = data.map(function(item, index) {
-        return (
-          <div key={index}>
-            <Article data={item} />
-          </div>
-        )
-      })
-    } else {
-      newsTemplate = <p>К сожалению, постов нет</p>
-    }
-
-    return (
-      <div className='news'>
-        {newsTemplate}
-      </div>
-    );
+  render() {
+		return(
+			<div className="container">
+				<div className="container1">
+					{this.state.posts.slice(0,this.state.num)}
+				</div>
+        <div className="more_btn_container">
+				<button className="more_btn" onClick={this.handleClick}>
+					More posts
+				</button>
+				</div>
+				</div>
+		)
   }
-});
+	
+	handleClick(){
+		console.log('clicked');
+		this.setState({
+			num: this.state.num + 3
+		})
+	}
+}
+
+
 
 var App = React.createClass({
   render: function() {
     return (
       <div className='app'>
-        
-        <News data={my_news} />
+				<Posts />
       </div>
     );
   }
@@ -75,3 +73,5 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
